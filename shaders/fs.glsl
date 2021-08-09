7fe0ead2a0c107d75e7dbc6c90f8067f60b2b0c9
Andrea Gerosa <61738886+Jerry98x@users.noreply.github.com>
2021-08-09 18:57:41 +0200

    Added basic shaders

---
+++ shaders/fs.glsl
@@ -0,0 +1,20 @@
+#version 300 es
+
+precision mediump float;
+
+in vec3 fsPos;
+in vec3 fsNormal;
+in vec2 uvCoordinate;
+
+out vec4 outColor;
+
+uniform vec3 mDiffColor; //material diffuse color 
+uniform vec3 lightDirection; // directional light direction vec
+uniform vec3 lightColor; //directional light color 
+
+void main() {
+
+  vec3 nNormal = normalize(fsNormal);
+  vec3 lambertColor = mDiffColor * lightColor * dot(-lightDirection,nNormal);
+  outColor = vec4(clamp(lambertColor, 0.00, 1.0),1.0);
+}
---
+++ shaders/vs.glsl
@@ -0,0 +1,20 @@
+#version 300 es
+
+in vec3 inPosition;
+in vec3 inNormal;
+in vec2 a_uv;
+
+out vec3 fsNormal;
+out vec3 fsPos;
+out vec2 uvCoordinate;
+
+uniform mat4 matrix; 
+uniform mat4 nMatrix;     //matrix to transform normals
+uniform mat4 pMatrix;
+
+void main() {
+    uvCoordinate = a_uv;
+    fsNormal = mat3(nMatrix) * inNormal; 
+    fsPos = (pMatrix * vec4(inPosition, 1.0)).xyz;
+    gl_Position = matrix * vec4(inPosition, 1.0);
+}
